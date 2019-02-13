ff3e60ad4ab9a1dfad99ded249d8ebb3e60d7248
Shin Kicker Timmy <calebl1994@gmail.com>
2019-02-12 19:21:28 -0600

    Add files via upload

---
+++ MapTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTest : MonoBehaviour
+{
+
+    public GameObject sprite;
+
+    // Start is called before the first frame update
+    void Start () {
+  		MapController mappie = new MapController(20, 20);
+      mappie.makeList();
+      mappie.placeBuildings();
+  		//mappie.printMap();
+      char [,] myMap = mappie.getMap();
+
+      GenerateMap (myMap);
+  	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void GenerateMap (char [,] map)
+    {
+      for (int x = 0; x < 20; x++)
+      {
+        for (int y = 0; y < 20; y++)
+        {
+          if (map [x, y] == 'B')
+          {
+            Instantiate (sprite, new Vector3 (-10 + x, -10 + y), Quaternion.identity);
+          }
+        }
+      }
+    }
+}
